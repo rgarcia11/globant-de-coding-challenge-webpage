d1a1bdce5a96d1557742bef3cc9cfb8fd3feb7c8
Rogelio Garcia <rogelio.garciae@perficient.com>
2023-10-24 19:07:01 -0500

    9: Apply design to landing

--- employees_site/lib/ui/screens/challenge_landing_screen.dart
+++ employees_site/lib/ui/screens/challenge_landing_screen.dart
@@ -40,46 +40,51 @@ class _ChallengeLandingScreenState extends State<ChallengeLandingScreen> {
             height: double.infinity,
             width: double.infinity,
           ),
-          const Row(
-            // direction: isScreenWide ? Axis.horizontal : Axis.vertical,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child: Text.rich(
-                  TextSpan(
-                      style: TextStyle(
-                        fontSize: 48.0,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: 'Let us ',
-                          style: TextStyle(
-                            shadows: [
-                              Shadow(
-                                  color: Colors.white, offset: Offset(0, -5.0))
-                            ],
-                            color: Colors.transparent,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Color(0xFFa0f4a4),
-                            decorationThickness: 2.0,
-                          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 140.0),
+            child: Row(
+              // direction: isScreenWide ? Axis.horizontal : Axis.vertical,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Text.rich(
+                    TextSpan(
+                        style: TextStyle(
+                          fontSize: 48.0,
                         ),
-                        TextSpan(
-                          text: 'hire you',
-                          style: TextStyle(
-                            shadows: [
-                              Shadow(
-                                  color: Colors.white, offset: Offset(0, -5.0))
-                            ],
-                            color: Colors.transparent,
+                        children: [
+                          TextSpan(
+                            text: 'Let us ',
+                            style: TextStyle(
+                              shadows: [
+                                Shadow(
+                                    color: Colors.white,
+                                    offset: Offset(0, -5.0))
+                              ],
+                              color: Colors.transparent,
+                              decoration: TextDecoration.underline,
+                              decorationColor: Color(0xFFa0f4a4),
+                              decorationThickness: 2.0,
+                            ),
                           ),
-                        ),
-                      ]),
-                ),
-              ), // Araboto font
-              NewEmployeeForm(landing: true),
-            ],
+                          TextSpan(
+                            text: 'hire you',
+                            style: TextStyle(
+                              shadows: [
+                                Shadow(
+                                    color: Colors.white,
+                                    offset: Offset(0, -5.0))
+                              ],
+                              color: Colors.transparent,
+                            ),
+                          ),
+                        ]),
+                  ),
+                ), // Araboto font
+                NewEmployeeForm(landing: true),
+              ],
+            ),
           ),
         ],
       ),
